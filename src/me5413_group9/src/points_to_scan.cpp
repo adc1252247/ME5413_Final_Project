dad2634776666b8d3828930ef3896d2879bb39d0
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2026-04-12 01:45:04 +0800

    Worked much better using only height

--- src/me5413_group9/src/points_to_scan.cpp
+++ src/me5413_group9/src/points_to_scan.cpp
@@ -13,7 +13,10 @@
  *   ring:      offset 16
  *   time:      offset 18
  * 
- * Change to: Max height difference of +/-0.05cm from 0.385, max linear distance of 7.2
+ * 0.385 +/- 0.005 seems reasonable for Velodyne 16
+ * 0.42 +/- 0.01 seems reasonable for Velodyne 32
+ * 
+ * Change to: Max height difference of +/-0.005cm from 0.385, max linear distance of 7.2
  */
 #include <array>
 #include <cmath>
@@ -22,6 +25,7 @@
 #include <vector>
 
 #include "ros/ros.h"
+#include <ros/master.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
@@ -37,7 +41,10 @@ inline bool value_between(float v, float lower, float upper) {
 
 void publish_scan(const sensor_msgs::PointCloud2ConstPtr& msg) {
     /// ::: "CONFIG"
-    float MAX_RANGE = 5.0;
+    const float MAX_RANGE    = 9.0;
+    const float MAX_DRIVABLE = 0.02;
+    const float ERR_DISTANCE = 0.005;
+    const float FLOOR        = -0.42;
 
     /// ::: 0.i) Format and publish the LaserScan message
     sensor_msgs::LaserScan scan_msg;
@@ -67,9 +74,9 @@ void publish_scan(const sensor_msgs::PointCloud2ConstPtr& msg) {
     intensities.assign(360, std::numeric_limits<float>::infinity());
 
     /// ::: 0.iii) Initialize the points in case a point not published
-    std::array<std::array<std::pair<float, float>, 16>, 360> scan_layers;
+    std::array<std::array<std::pair<float, float>, 32>, 360> scan_layers;
     for ( size_t a = 0; a < 360; a++ ) {
-        for ( size_t i = 0; i < 16; i++ ) {
+        for ( size_t i = 0; i < 32; i++ ) {
             scan_layers[a][i].first  = std::numeric_limits<float>::quiet_NaN();
             scan_layers[a][i].second = std::numeric_limits<float>::quiet_NaN();
         }
@@ -81,15 +88,20 @@ void publish_scan(const sensor_msgs::PointCloud2ConstPtr& msg) {
         throw std::runtime_error("<PointCloud2>(msg)->point_step != 22");
     }
     if ( 22 * msg->width * msg->height < msg->data.size() ) {
-        ROS_ERROR("PointCloud2 message 'size' expected %d, received %d!", 22 * msg->width * msg->height, (int)msg->data.size());
+        ROS_ERROR("PointCloud2 malformed message 'size' expected %d, received %d!", 22 * msg->width * msg->height, (int)msg->data.size());
         throw std::runtime_error("<PointCloud2>(msg)->data.size() too low!");
     }
-
 
     /// ::: 1) Compute x-/y- plane distance to point, and keep z- height    
     size_t max_bin = 0, min_bin = 360;
     for ( size_t i = 0; i < msg->width * msg->height; i++ ) {
         uint16_t ring = *(uint16_t*)(&msg->data[i * msg->point_step + 16]);
+
+        if ( ring > 32 ) {
+            ROS_ERROR("PointCloud2 too many rings, expected 32, received %d or more!", ring);
+            continue; // Skip this ring...
+        }
+
         float x = *(float*)(&msg->data[i * msg->point_step + 0]);
         float y = *(float*)(&msg->data[i * msg->point_step + 4]);
         float z = *(float*)(&msg->data[i * msg->point_step + 8]);
@@ -113,65 +125,43 @@ void publish_scan(const sensor_msgs::PointCloud2ConstPtr& msg) {
 
     /// ::: 2) Compute the furthest drivable distance for each 360 angle
     ///        Assumption: All obstacles only made of vertical walls
-    const float MAX_DRIVABLE = 10 * 2 * M_PI / 360; // Anything below 10 degrees is drivable
-    const float VERTICAL     = 85 * M_PI / 360; // Anything above 85 degrees is vertical
-
     for ( size_t a = 0; a < 360; a++ ) {
-        bool  unblocked = true;
+        float last_d   = 0;
+        bool unblocked = true;
 
-        float last_d    = 0.0;
-        float last_z    = -0.2617; // Height of velodyne from ground...
-                                   // Short baricades are at heigh 0.19 above the ground
+        for ( size_t r = 0; r < 32; r++ ) {
+            float d = scan_layers[a][r].first;
+            float z = scan_layers[a][r].second - FLOOR;
 
-        for ( size_t i = 0; i < 16; i++ ) {
-            float curr_d = scan_layers[a][i].first;
-            float curr_z = scan_layers[a][i].second;
 
-            if ( std::isnan(curr_d) || std::isnan(curr_z) ) {
+            if ( std::isnan(d) || std::isnan(z) )
                 continue;
-            }
-            // if ( curr_d > MAX_RANGE )
-            //     break;
 
-            float delta_d = curr_d - last_d;
-            float delta_z = curr_z - last_z;
+            if ( d > MAX_RANGE )
+                break;
 
-            float angle = atan2(delta_z, delta_d);
-
-            if ( /* i == 6 && */ a == 0 ) // DEBUG
-                ROS_INFO("(i: %d, a: %d) | H: %f | D: %f | A: %f", i, a, curr_z, curr_d, angle); 
-
-            // if ( a != 0 )
-            //     continue;
-            // ROS_INFO("Step (%d:%d) angle %f x %f -> %f / %f", a, i, curr_d, curr_z, angle, MAX_DRIVABLE);
-                        
-            // No wall/drop found yet
+            // Find first obstacle
             if ( unblocked ) {
-                // If drop too large, quit iterating
-                // Skip first iteration
-                if ( !value_between(angle, -MAX_DRIVABLE, MAX_DRIVABLE) ) {
-                    ranges[a]      = curr_d;
-                    intensities[a] = curr_z;
-
-                    if ( angle < 0 )
-                        break;
-
+                if ( fabs(z) > MAX_DRIVABLE ) {
+                    // ROS_INFO("(r: %d, a: %d) Blocked! | Z: %.3f | D: %.2f", r, a, z, d);
+                    
+                    ranges[a]      = d;
+                    intensities[a] = z;
+                    last_d    = d;
                     unblocked = false;
-                }
 
-                last_d = curr_d;
-                last_z = curr_z;
+                    if ( z < 0 )
+                        break;
+                }
             }
-            // Wall/drop already found
-            else {
-                // Top of wall found
-                if ( angle < VERTICAL ) {
-                    if ( a != 0 ) // DEBUG
-                    break;
-                }
 
+            // Find top of wall
+            else {
+                if ( fabs(last_d - d) <= ERR_DISTANCE )
+                    intensities[a] = z;
+                
                 else
-                    intensities[a] = curr_z;
+                    break;
             }
         }
 
@@ -181,6 +171,28 @@ void publish_scan(const sensor_msgs::PointCloud2ConstPtr& msg) {
     pub.publish(scan_msg);
 }
 
+
+
+/// ============= ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/// === Utils === ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+bool topic_in_use(const std::string& t) {
+    ros::NodeHandle nh;
+
+    ros::master::V_TopicInfo topics;
+    ros::master::getTopics(topics);
+
+    for ( const auto& topic: topics ) {
+        if ( topic.name == t )
+            return true;
+    }
+
+    return false;
+}
+
+
+
+/// ============ :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/// === Main === :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 int main(int argc, char** argv) {
     ros::init(argc, argv, "points_to_scan");
     ros::NodeHandle nh;
@@ -188,6 +200,11 @@ int main(int argc, char** argv) {
     std::string points_topic, scan_topic;
     nh.param<std::string>("points_topic", points_topic, "/mid/points");
     nh.param<std::string>("scan_topic", scan_topic, "/front/scan");
+
+    if ( topic_in_use(scan_topic) ) {
+        ROS_ERROR("Topic '%s' is already used!", scan_topic.c_str());
+        throw std::runtime_error("Topic for points_to_scan already in use!");
+    }
 
     ros::Subscriber sub = nh.subscribe(points_topic, 1, publish_scan);
     pub = nh.advertise<sensor_msgs::LaserScan>(scan_topic, 1);
