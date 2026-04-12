153edf8a0d7d074d2578dd564fd974da8351cae8
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2026-04-12 11:24:01 +0800

    Obsolete

--- src/me5413_group9/src/ferdi-stuff.cmake
+++ src/me5413_group9/src/ferdi-stuff.cmake
@@ -1,2 +1,2 @@
-add_executable(show_img ${CMAKE_CURRENT_LIST_DIR}/show_img.cpp) # ${COMMON_SOURCES}
-target_link_libraries(show_img ${catkin_LIBRARIES} ${OpenCV_LIBS})
+# add_executable(show_img ${CMAKE_CURRENT_LIST_DIR}/show_img.cpp) # ${COMMON_SOURCES}
+# target_link_libraries(show_img ${catkin_LIBRARIES} ${OpenCV_LIBS})
